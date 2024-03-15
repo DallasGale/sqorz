832999297a3123ecdfa983afb9eda6a10f17da02
Dallas Gale <dallasgale.digital@gmail.com>
2024-03-15 16:01:34 +1100

    wip

--- src/pages/api/sqorz/region_au.js
+++ src/pages/api/sqorz/region_au.js
@@ -11,9 +11,19 @@ export default async function handler(req, res) {
       throw new Error(`Request failed with status ${response.status}`);
     }
     const data = await response.text(); // or response.json() if the API returns JSON
+    // Set CORS headers
+    // Allow any domain (*) or specify to only allow requests from specific origins
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    // Specify which headers can be used in the request
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+
     res.status(200).send(data);
   } catch (error) {
-    console.error(error);
+    // Ensure CORS headers are set even on error responses
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.status(500).send("Proxy error");
   }
 }
